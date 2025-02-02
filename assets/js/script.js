8ccf84e64bbfdc651480d016ce1f066d7ff2e813
May Guan <maymayy08@users.noreply.github.com>
2025-02-02 15:56:18 +0000

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,60 @@
-var mainContentEl = document.getElementById("mainContent");
+// **Refer to HTML Elements**
+
+// Refer to whole page of Main Container 
+var mainContentEl = document.getElementById("mainContent"); 
+// The title 
 var titleEl = document.getElementById("pageTitle");
+// The input field for task description 
 var inputTaskEl = document.getElementById("inputTask");
+// Add task button 
 var addTaskEl = document.getElementById("addTask");
+// The input field for selecting a date
 var dateEl = document.getElementById("date");
 
+// Triggle the Add button when user click and create add function
 addTaskEl.addEventListener("click", add);
 
+// Grab the function from previous and use it here
 function add() {
+// Create variable and assign to input element, remove white spaces from both end for cleaner structure
   var input = inputTaskEl.value.trim();
+//   Create variable and assign to date element 
   var dueDate = dateEl.value;
+//   Get HTML element - The to do list ID
   var todoListEl = document.getElementById("todoList");
 
+  // Tell the user, if no task is enter, prompt the message 
   if (!input) {
     alert("Please enter a task");
+    // return nothing else after 
     return;
   }
-
+// Tell the user, if no date selected, prompt a message 
   if (!dueDate) {
     alert("Please select a date");
+    // return nothing else after
     return;
   }
 
+//   Create a new HTML Dom element of list 
   var newParagraph = document.createElement("li");
+//   Create class name of task 
   newParagraph.classList.add("task");
+//   Given the element a name 
   newParagraph.textContent = input;
+//   Get the parent todoListEL Div from HTML element and create child element then assign to parent
   todoListEl.appendChild(newParagraph);
+//   clear user input 
   inputTaskEl.value = "";
-
+// create delete button 
   var deleteTaskEl = document.createElement("button");
+//   create delete class 
   deleteTaskEl.classList.add("delete-btn");
+//   Give element a name 
   deleteTaskEl.textContent = "🗑️";
-
+// create a event listner and create a function inside 
   deleteTaskEl.addEventListener("click", handleDelete);
-
+// handle function from event listner then trigger it, when user click delete. it remove the input from the list and then prompt a message remindar
   function handleDelete() {
     if (newParagraph) {
       todoListEl.removeChild(newParagraph);
@@ -40,9 +62,11 @@ function add() {
       return;
     }
   }
-
+// create edit button 
   var editTaskEl = document.createElement("button");
+// Create a class for edit 
   editTaskEl.classList.add("edit-btn");
+//   Give the element a name 
   editTaskEl.textContent = "📝";
 
   // Event listener for the Edit button
@@ -83,12 +107,16 @@ function add() {
     }
   });
 
-   
+// Create complete button
   var completeTaskEl = document.createElement("button")
+// Create a class for complete button for styling 
   completeTaskEl.classList.add("complete-Btn");
+//   Assign a name for the button 
   completeTaskEl.textContent = "✅";
 
+//   create event listner 
   completeTaskEl.addEventListener("click", function() {
+    // create a class for styling and then triggle the input field, when user click on complete, the input field text strikethrough 
     newParagraph.classList.toggle("strikethrough");
   });
 
@@ -99,8 +127,6 @@ function add() {
   newParagraph.appendChild(completeTaskEl);
 
   
-
-
   // Clear the input fields after adding the task
   inputTaskEl.value = "";
   dateEl.value = "";
